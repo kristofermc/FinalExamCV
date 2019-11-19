c9bfd65a586278ad2c2c2aa0707147cb59bbfe41
kristofermc <kris.jussi@gmail.com>
2019-11-19 12:57:52 +0200

    potentially complete app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 
-//  { path: '', loadChildren: './login/login.module#LoginPageModule' },
-//  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: '', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
   { path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)},
   { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },
   { path: 'cv', loadChildren: './cv/cv.module#CvPageModule' },
